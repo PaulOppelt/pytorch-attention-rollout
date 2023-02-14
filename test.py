288abf4dead2d37df5b758c1abccd6d5115066c0
pauloppelt <pauloppelt@gmx.net>
2023-02-14 16:43:37 +0100

    change structure for readability

--- test.py
+++ test.py
@@ -2,20 +2,27 @@
 from torch.nn import TransformerEncoder
 import torch
 import copy
-from EncoderLayer import WrapperEncoderLayer
+from custom import WrapperEncoderLayer
 import torch.nn as nn
+import torch.functional as F
 
 
 if __name__=="__main__":
     # set random seed
     torch.manual_seed(0)
 
+    seq_lenght = 32
+    d_model = 12
+    nhead = 4
+    dim_feedforward = 12
+    num_layers = 2
+
     # initialize pytorch transformer model with Encoder layer.
-    encoder_layer = nn.TransformerEncoderLayer(d_model=12, nhead=4, dim_feedforward=12)
-    transformer = nn.TransformerEncoder(encoder_layer = encoder_layer, num_layers=2)
+    encoder_layer = nn.TransformerEncoderLayer(d_model=d_model, nhead=nhead, dim_feedforward=dim_feedforward)
+    transformer = nn.TransformerEncoder(encoder_layer = encoder_layer, num_layers=num_layers)
 
     # initialize custom transformer model with custom Encoder layer to extraxt attention weights.
-    custom_layer  = WrapperEncoderLayer(d_model=12,nhead=4, dim_feedforward=12)
+    custom_layer  = WrapperEncoderLayer(d_model=d_model,nhead=nhead, dim_feedforward=dim_feedforward)
     custom_transformer = nn.TransformerEncoder(encoder_layer=custom_layer, num_layers=2)
 
     # copy state dict to custom transformer
@@ -25,7 +32,47 @@ if __name__=="__main__":
     transformer.eval()
     custom_transformer.eval()
 
-    # define input
-    input = torch.randn(512, 32, 12)
+    bsz = 512
+    device = "cpu"
+    label_ind = 1 # label towards which we want to calculate the attention rollout.
+
+    input_batch = torch.randn(512, 32, 12)
     print(transformer(input))
     print(custom_transformer(input))
+
+    # calculate rollout:
+    custom_transformer.zero_grad()
+
+    out = custom_transformer(input_batch)
+    mask = torch.zeros(out.shape).to(device)
+    mask[:,label_ind] = 1
+    loss = (mask*out).sum()
+    # by calling loss backwards the attention weights and the gradients are stored in the custom layer.
+    loss.backward()
+
+    with torch.no_grad():
+        #rollout = torch.eye(seq_lenght)  init rollout is the identity martix. this models the residual connection.
+        result = torch.eye(seq_lenght).to(device) # init rollout is the identity martix. this models the residual connection.
+        for layer in custom_transformer.layers:
+            # fetch attention weights and their gradients w.r.t the class loss.
+            gradient = layer.gradients[-1].view(bsz, nhead, seq_lenght, seq_lenght)
+            attention = layer.weight_attn[-1].view(bsz, nhead, seq_lenght, seq_lenght)
+            # get the maximum elements across the attention heads.
+            inter_ = torch.max(gradient*attention, dim=1)[0] 
+            # apply relu activation in order to have only positive values.)
+            inter_ = nn.ReLU()(inter_)                 
+            # normalize the attention matrix. 
+
+            rollout = (inter_ + torch.eye(seq_lenght))/2
+            rollout = F.normalize(rollout, dim=1)
+            result = torch.matmul(rollout, result)
+
+            """NOTE: weights_attn and gradiens are initialized as lists even though they only store one
+            element. this is because register hook does not support value assignment (as far as I know).
+            Therefore i free the lists in order to save memory.
+            """
+            layer.weight_attn = []
+            layer.gradients = []
+
+        # map the attention rollout to the input tokens.
+        token_att = result @ torch.ones(seq_lenght)
