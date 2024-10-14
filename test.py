a1bc276c0c088d891cd27063eaed3daf779a1356
pauloppelt <pauloppelt@gmx.net>
2024-10-14 22:58:09 +0200

    make rollout cleaner

--- test.py
+++ test.py
@@ -36,27 +36,31 @@ if __name__=="__main__":
     device = "cpu"
     label_ind = 1 # label towards which we want to calculate the attention rollout.
 
+    # check if custom layer and pytorch layer return the same output
     input_batch = torch.randn(512, 32, 12)
     print(transformer(input))
     print(custom_transformer(input))
 
-    # calculate rollout:
+    # do a forward pass though the custom transformer in order to store the attention weights.
     custom_transformer.zero_grad()
 
     out = custom_transformer(input_batch)
+    # some kind of classification head on top of the transformer output. needs to be defined by the user.
+    #out = outputlayer(out) <<< to implement by the user.
     mask = torch.zeros(out.shape).to(device)
     mask[:,label_ind] = 1
     loss = (mask*out).sum()
     # by calling loss backwards the attention weights and the gradients are stored in the custom layer.
     loss.backward()
 
+    # calculate rollout from stored attention weights and gradients.
     with torch.no_grad():
         #rollout = torch.eye(seq_lenght)  init rollout is the identity martix. this models the residual connection.
         result = torch.eye(seq_lenght).to(device) # init rollout is the identity martix. this models the residual connection.
         for layer in custom_transformer.layers:
             # fetch attention weights and their gradients w.r.t the class loss.
-            gradient = layer.gradients[-1].view(bsz, nhead, seq_lenght, seq_lenght)
-            attention = layer.weight_attn[-1].view(bsz, nhead, seq_lenght, seq_lenght)
+            gradient = layer.gradients.pop().view(bsz, nhead, seq_lenght, seq_lenght)
+            attention = layer.weight_attn.pop().view(bsz, nhead, seq_lenght, seq_lenght)
             # get the maximum elements across the attention heads.
             inter_ = torch.max(gradient*attention, dim=1)[0] 
             # apply relu activation in order to have only positive values.)
@@ -71,8 +75,8 @@ if __name__=="__main__":
             element. this is because register hook does not support value assignment (as far as I know).
             Therefore i free the lists in order to save memory.
             """
-            layer.weight_attn = []
-            layer.gradients = []
+            #layer.weight_attn = []
+            #layer.gradients = []
 
         # map the attention rollout to the input tokens.
         token_att = result @ torch.ones(seq_lenght)
